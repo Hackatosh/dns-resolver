37235b5e7db1016abed8ce2067aa02216822e14c
Hackatosh <edouardbenauw@gmail.com>
2023-05-19 18:08:31 +0200

    Code domain name encoding

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dns-resolver
 
 Based on https://implement-dns.wizardzines.com/index.html
+http://www.tcpipguide.com/free/t_DNSMessageHeaderandQuestionSectionFormat.htm
+https://www.zytrax.com/books/dns/ch15/#question
 
 To run, use the following command and whitelist the dns-resolver folder in your favorite antivirus :
 ```
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func encode_domain_name(domain_name string) bytes.Buffer {
+	encoded_result := bytes.Buffer{}
+	splitted := strings.Split(domain_name, ".")
+	for _, part := range splitted {
+		encoded_result.Write([]byte{byte(len(part))})
+		encoded_result.WriteString(part)
+	}
+	encoded_result.Write([]byte{byte(0)})
+	return encoded_result
+}
+
+// Todo : verify that the domain name is in ascii
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(encode_domain_name("google.com"))
 }
